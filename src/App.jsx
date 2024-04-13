2ae6e48812001b4201a85d4ae46d5e44ce384792
shujashah <shuja.ashraf11@gmail.com>
2024-04-13 21:06:28 +0530

    implemented styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">Haven Resort</Heading>
+        <Heading as="h2">Check in and Check out</Heading>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  );
 };
 
 export default App;
