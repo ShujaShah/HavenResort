7c6e99b5db2720817a7ac5e98a1bd7641f4a7d1e
shujashah <shuja.ashraf11@gmail.com>
2024-04-14 12:57:51 +0530

    implemented a styled custom Row

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 const App = () => {
@@ -13,12 +13,23 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Haven Resort</Heading>
-        <Heading as="h2">Check in and Check out</Heading>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Haven Resort</Heading>
+            <div>
+              <Heading as="h2">Check in and Check out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
